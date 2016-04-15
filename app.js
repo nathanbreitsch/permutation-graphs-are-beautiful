98e46a27f558c9400ab2eec3dc400cab8aaeba22
nathan breitsch <nathanbreitsch@gmail.com>
2016-04-15 16:41:09 -0400

    done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var TRANSITION_TIME = 750;
 
+var palette = d3.scale.category20();
 
 var range = function(n){
   var points = [];
@@ -12,8 +13,9 @@ var range = function(n){
 function ViewModel(){
   var self = this;
   var modelGraphSVG = null;
+  self.intersectionGraphSVG = null;
 
-  self.permutationString = ko.observable('1,5,3,2,4');
+  self.permutationString = ko.observable('3,2,1,7,6,5,4');
 
   self.permutation = ko.computed(function(){
 
@@ -35,6 +37,7 @@ function ViewModel(){
 
   self.permutation.subscribe(function(newValue, oldValue){
     self.drawModelGraph();
+    self.drawIntersectionGraph();
   });
 
 
@@ -87,18 +90,80 @@ function ViewModel(){
       .attr("y2", function(d, i){return height * 0.8})
       .attr("stroke-width", 2)
       .attr("class", "edge")
-      .attr("stroke", "black");
+      .attr("stroke", function(d, i){return palette(i%20)});
 
       lines.exit().remove();
       topCircles.exit().remove();
       bottomCircles.exit().remove();
   };
 
+  self.drawIntersectionGraph = function(){
+    var permutation = self.permutation();
+    var intersectionGraph = computeIntersectionGraph(self.permutation());
+    var width = $("#model-graph").width();
+    var height = $(window).height() * 0.8;
+    var radius = 10;
+    var linkDistance = 30;
+    var charge = -100;
+
+    self.intersectionGraphSVG.attr('width', width).attr('height', height);
+    var forceField = d3.layout.force()
+      .charge(charge)
+      .linkDistance(linkDistance)
+      .size([width, height]);
+
+    forceField.nodes(intersectionGraph.getNodes())
+      .links(intersectionGraph.getEdges())
+      .start();
+
+    var links = self.intersectionGraphSVG.selectAll(".link")
+      .data(intersectionGraph.getEdges());
+
+    links.enter().append("line");
+
+    links.attr("stroke-width", 2)
+      .attr("stroke", "black")
+      .attr("class", "link");
+
+    links.exit().remove();
+
+    var nodes = self.intersectionGraphSVG.selectAll(".node")
+      .data(intersectionGraph.getNodes());
+
+    nodes.enter().append("circle");
+
+    nodes.attr("class", "node")
+      .attr("r", radius)
+      .call(forceField.drag);
+
+    nodes.exit().remove();
+
+    forceField.on("tick", function() {
+
+      links.attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+
+      nodes.attr("cx", function(d) { return d.x; })
+          .style('fill', function(d, i){return palette(i%20)})
+          .attr("cy", function(d) { return d.y; });
+
+    });
+
+  };
 }
 
 $(function(){
   var viewModel = new ViewModel();
   viewModel.modelGraphSVG = d3.select("#model-graph").append('svg');
+  viewModel.intersectionGraphSVG = d3.select("#intersection-graph").append('svg');
   viewModel.drawModelGraph();
+  viewModel.drawIntersectionGraph();
+
+  $(window).on('resize', function(){
+    viewModel.drawModelGraph();
+    viewModel.drawIntersectionGraph();
+  });
   ko.applyBindings(viewModel);
 });
--- logic.js
+++ logic.js
@@ -6,7 +6,7 @@ var computeIntersectionGraph = function(permutation){
   for(var i = 0; i < permutation.length; i++){
     for(var j = i + 1; j < permutation.length; j++){
       if(linesIntersect(i, permutation[i], j, permutation[j])){
-        intersectionGraph.setSymmetricAdjacency(i,j);
+        intersectionGraph.setAdjacency(i,j);
       }
     }
   }
@@ -16,31 +16,36 @@ var computeIntersectionGraph = function(permutation){
 //a1, a2 are indices in permutation (nodes on top line of model graph)
 //b1, b2 are images in permutation of a1 and a2 (nodes on bottom line of model graph)
 var linesIntersect = function(a1, b1, a2, b2){
-  return (a1 - a2) * (b1 - b2); // iff lines cross, this product will be negative
+  return (a1 - a2) * (b1 - b2) < 0; // iff lines cross, this product will be negative
 }
 
 //Matrix representation of graph
 //construct from n nodes
 function Graph(n){
   var self = this;
-  self.matrix = [];
-  for(var i = 0; i < n; i++){
-    var row = [];
-    for(var j = 0; j < n; j++){
-      row.push(false)
-    }
-    self.matrix.push(row);
-  }
+  self.nodes = range(n).map(function(d){return {name: d}});
+  self.edges = [];
 };
 
-//return true iff i adj to j
-Graph.prototype.adjacent = function(i, j){
-  return self.matrix[i][j];
-};
+
+function Edge(i, j){
+  this.source = i;
+  this.target = j;
+}
+
 
 //keeping code simple at expense of a little memory
 //ideally, we only store edge list or trangular matrix
-Graph.prototype.setSymmetricAdjacency = function(i,j){
-  self.matrix[i][j] = true;
-  self.matrix[j][i] = true;
+Graph.prototype.setAdjacency = function(i,j){
+  this.edges.push(new Edge(i,j));
 };
+
+Graph.prototype.getNodes = function(){ return this.nodes; }
+Graph.prototype.getEdges = function(){ return this.edges; }
+
+var range = function(n){
+  var arrayRtn = [];
+  for(var i = 0; i < n; i++){
+    arrayRtn.push(i);
+  }
+}
